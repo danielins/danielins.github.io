96841a7ed2ab067e70e9a2f9ce7c4ffeb7fb735c
Daniel Lins <daniel.alves.321@gmail.com>
2023-01-26 18:39:15 -0300

    node config test

--- next.config.js
+++ next.config.js
@@ -3,12 +3,12 @@ const isGithubActions = process.env.GITHUB_ACTIONS || false
 let assetPrefix = null
 let basePath = null
 
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+// if (isGithubActions) {
+//   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
 
-  assetPrefix = `/${repo}/aaa`
-  basePath = `/${repo}aaaa`
-}
+//   assetPrefix = `/${repo}/aaa`
+//   basePath = `/${repo}aaaa`
+// }
 
 const nextConfig = {
   images: {
